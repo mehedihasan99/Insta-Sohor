1e8c5792731f24eb09641de59ee299ea08d9652f
mehedihasan99 <hmhasan.mehedi2003@gmail.com>
2022-03-10 23:18:09 +0600

    user image issue solve

--- js/app.js
+++ js/app.js
@@ -51,9 +51,8 @@ const switchTab = (id) => {
 };
 
 const createPost = (post) => {
-  // console.log(post);
     const image = post.image;
-    // console.log(image);
+    const userImage = post.userImage;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
     div.innerHTML = `
@@ -64,7 +63,7 @@ const createPost = (post) => {
                     target="_blank"
                     class="post__avatar"
                   >
-                    <img src="${image}" alt="User Picture" />
+                    <img src="${userImage}" alt="User Picture" />
                   </a>
                   <a href="#" class="post__user">phero</a>
                 </div>
